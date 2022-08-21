ff0d1f746ed5c4ceed3f00954f59187c228ed844
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-08-21 20:29:27 +0800

    Update package.json with new dependencies

--- src/source/Lib/TLibEncoder/TEncTemporalFilter.h
+++ src/source/Lib/TLibEncoder/TEncTemporalFilter.h
@@ -98,3 +98,103 @@ struct TemporalFilterSourcePicInfo
 {
   TemporalFilterSourcePicInfo() : picBuffer(), mvs(), origOffset(0) { }
   TComPicYuv            picBuffer;
+  Array2D<MotionVector> mvs;
+  Int                   origOffset;
+};
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+class TEncTemporalFilter
+{
+public:
+   TEncTemporalFilter();
+  ~TEncTemporalFilter() {}
+
+  void init(const Int frameSkip,
+            const Int inputBitDepth[MAX_NUM_CHANNEL_TYPE],
+            const Int MSBExtendedBitDepth[MAX_NUM_CHANNEL_TYPE], const Int InternalBitDepth[MAX_NUM_CHANNEL_TYPE],
+            const Int width,
+            const Int height,
+            const Int *pad,
+            const Int frames,
+            const Bool Rec709,
+            const std::string &filename,
+            const ChromaFormat inputChromaFormatIDC,
+            const InputColourSpaceConversion colorSpaceConv,
+            const Int qp,
+            const Int GOPSize,
+            const std::map<Int, Double> &temporalFilterStrengths,
+            const Int pastRefs,
+            const Int futureRefs,
+            const Int firstValidFrame,
+#if !JVET_Y0077_BIM
+            const Int lastValidFrame);
+#else
+            const Int lastValidFrame,
+            const Bool mctfEnabled,
+            std::map<Int, Int*> *adaptQPmap,
+            const Bool bimEnabled);
+#endif
+
+  Bool filter(TComPicYuv *orgPic, Int frame);
+
+private:
+  // Private static member variables
+  static const Double s_chromaFactor;
+  static const Double s_sigmaMultiplier;
+  static const Double s_sigmaZeroPoint;
+  static const Int s_motionVectorFactor;
+  static const Int s_padding;
+  static const Int s_interpolationFilter[16][8];
+#if JVET_V0056_MCTF
+  static const Double s_refStrengths[2][4];
+#else
+  static const Double s_refStrengths[2][2];
+#endif
+#if JVET_Y0077_BIM
+  static const Int s_cuTreeThresh[4];
+#endif
+
+  // Private member variables
+  Int m_FrameSkip;
+  std::string m_inputFileName;
+  Int m_inputBitDepth[MAX_NUM_CHANNEL_TYPE];
+  Int m_MSBExtendedBitDepth[MAX_NUM_CHANNEL_TYPE];
+  Int m_internalBitDepth[MAX_NUM_CHANNEL_TYPE];
+  ChromaFormat m_chromaFormatIDC;
+  Int m_sourceWidth;
+  Int m_sourceHeight;
+  Int m_QP;
+  Int m_GOPSize;
+  std::map<Int, Double> m_temporalFilterStrengths;
+  Int m_sourcePadding[2];
+  Int m_framesToBeEncoded;
+  Bool m_bClipInputVideoToRec709Range;
+  InputColourSpaceConversion m_inputColourSpaceConvert;
+  Int m_pastRefs;
+  Int m_futureRefs;
+  Int m_firstValidFrame;
+  Int m_lastValidFrame;
+#if JVET_Y0077_BIM
+  Bool m_mctfEnabled;
+  Bool m_bimEnabled;
+  Int m_numCTU;
+  std::map<Int, Int*> *m_ctuAdaptQP;
+#endif
+
+  // Private functions
+  Void subsampleLuma(const TComPicYuv &input, TComPicYuv &output, const Int factor = 2) const;
+  Int motionErrorLuma(const TComPicYuv &orig, const TComPicYuv &buffer, const Int x, const Int y, Int dx, Int dy, const Int bs, const Int besterror = 8 * 8 * 1024 * 1024) const;
+  Void motionEstimationLuma(Array2D<MotionVector> &mvs, const TComPicYuv &orig, const TComPicYuv &buffer, const Int bs,
+      const Array2D<MotionVector> *previous=0, const Int factor = 1, const Bool doubleRes = false) const;
+  Void motionEstimation(Array2D<MotionVector> &mvs, const TComPicYuv &orgPic, const TComPicYuv &buffer, const TComPicYuv &origSubsampled2, const TComPicYuv &origSubsampled4) const;
+
+#if JVET_V0056_MCTF
+  Void bilateralFilter(const TComPicYuv &orgPic, std::deque<TemporalFilterSourcePicInfo> &srcFrameInfo, TComPicYuv &newOrgPic, Double overallStrength) const;
+#else
+  Void bilateralFilter(const TComPicYuv &orgPic, const std::deque<TemporalFilterSourcePicInfo> &srcFrameInfo, TComPicYuv &newOrgPic, Double overallStrength) const;
+#endif
+  Void applyMotion(const Array2D<MotionVector> &mvs, const TComPicYuv &input, TComPicYuv &output) const;
+}; // END CLASS DEFINITION TEncTemporalFilter
