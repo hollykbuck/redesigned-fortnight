8700c2195047b3bb6ccbbe8747e6a7f7815b4352
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-09-01 20:43:12 +0800

    Update dependencies to latest versions

--- src/source/Lib/TLibCommon/TComSlice.h
+++ src/source/Lib/TLibCommon/TComSlice.h
@@ -98,3 +98,103 @@ public:
   Void    setCheckLTMSBPresent(Int bufferNum, Bool b );
   Bool    getCheckLTMSBPresent(Int bufferNum) const;
 
+  Int     getUsed(Int bufferNum) const;
+  Int     getDeltaPOC(Int bufferNum) const;
+  Int     getPOC(Int bufferNum) const;
+  Int     getNumberOfPictures() const;
+
+  Void    setNumberOfNegativePictures(Int number)      { m_numberOfNegativePictures = number; }
+  Int     getNumberOfNegativePictures() const          { return m_numberOfNegativePictures;   }
+  Void    setNumberOfPositivePictures(Int number)      { m_numberOfPositivePictures = number; }
+  Int     getNumberOfPositivePictures() const          { return m_numberOfPositivePictures;   }
+  Void    setNumberOfLongtermPictures(Int number)      { m_numberOfLongtermPictures = number; }
+  Int     getNumberOfLongtermPictures() const          { return m_numberOfLongtermPictures;   }
+
+  Void    setInterRPSPrediction(Bool flag)             { m_interRPSPrediction = flag;         }
+  Bool    getInterRPSPrediction() const                { return m_interRPSPrediction;         }
+  Void    setDeltaRIdxMinus1(Int x)                    { m_deltaRIdxMinus1 = x;               }
+  Int     getDeltaRIdxMinus1() const                   { return m_deltaRIdxMinus1;            }
+  Void    setDeltaRPS(Int x)                           { m_deltaRPS = x;                      }
+  Int     getDeltaRPS() const                          { return m_deltaRPS;                   }
+  Void    setNumRefIdc(Int x)                          { m_numRefIdc = x;                     }
+  Int     getNumRefIdc() const                         { return m_numRefIdc;                  }
+
+  Void    setRefIdc(Int bufferNum, Int refIdc);
+  Int     getRefIdc(Int bufferNum) const ;
+
+  Void    sortDeltaPOC();
+  Void    printDeltaPOC() const;
+};
+
+/// Reference Picture Set set class
+class TComRPSList
+{
+private:
+  std::vector<TComReferencePictureSet> m_referencePictureSets;
+
+public:
+                                 TComRPSList()                                            { }
+  virtual                        ~TComRPSList()                                           { }
+
+  Void                           create  (Int numberOfEntries)                            { m_referencePictureSets.resize(numberOfEntries);         }
+  Void                           destroy ()                                               { }
+
+
+  TComReferencePictureSet*       getReferencePictureSet(Int referencePictureSetNum)       { return &m_referencePictureSets[referencePictureSetNum]; }
+  const TComReferencePictureSet* getReferencePictureSet(Int referencePictureSetNum) const { return &m_referencePictureSets[referencePictureSetNum]; }
+
+  Int                            getNumberOfReferencePictureSets() const                  { return Int(m_referencePictureSets.size());              }
+};
+
+/// SCALING_LIST class
+class TComScalingList
+{
+public:
+             TComScalingList();
+  virtual    ~TComScalingList()                                                 { }
+  Int*       getScalingListAddress(UInt sizeId, UInt listId)                    { return &(m_scalingListCoef[sizeId][listId][0]);            } //!< get matrix coefficient
+  const Int* getScalingListAddress(UInt sizeId, UInt listId) const              { return &(m_scalingListCoef[sizeId][listId][0]);            } //!< get matrix coefficient
+  Void       checkPredMode(UInt sizeId, UInt listId);
+
+  Void       setRefMatrixId(UInt sizeId, UInt listId, UInt u)                   { m_refMatrixId[sizeId][listId] = u;                         } //!< set reference matrix ID
+  UInt       getRefMatrixId(UInt sizeId, UInt listId) const                     { return m_refMatrixId[sizeId][listId];                      } //!< get reference matrix ID
+
+  const Int* getScalingListDefaultAddress(UInt sizeId, UInt listId);                                                                           //!< get default matrix coefficient
+  Void       processDefaultMatrix(UInt sizeId, UInt listId);
+
+  Void       setScalingListDC(UInt sizeId, UInt listId, UInt u)                 { m_scalingListDC[sizeId][listId] = u;                       } //!< set DC value
+  Int        getScalingListDC(UInt sizeId, UInt listId) const                   { return m_scalingListDC[sizeId][listId];                    } //!< get DC value
+
+  Void       setScalingListPredModeFlag(UInt sizeId, UInt listId, Bool bIsDPCM) { m_scalingListPredModeFlagIsDPCM[sizeId][listId] = bIsDPCM; }
+  Bool       getScalingListPredModeFlag(UInt sizeId, UInt listId) const         { return m_scalingListPredModeFlagIsDPCM[sizeId][listId];    }
+
+  Void       checkDcOfMatrix();
+  Void       processRefMatrix(UInt sizeId, UInt listId , UInt refListId );
+  Bool       xParseScalingList(const std::string &fileName);
+  Void       setDefaultScalingList();
+  Bool       checkDefaultScalingList();
+
+private:
+  Void       outputScalingLists(std::ostream &os) const;
+  Bool             m_scalingListPredModeFlagIsDPCM [SCALING_LIST_SIZE_NUM][SCALING_LIST_NUM]; //!< reference list index
+  Int              m_scalingListDC                 [SCALING_LIST_SIZE_NUM][SCALING_LIST_NUM]; //!< the DC value of the matrix coefficient for 16x16
+  UInt             m_refMatrixId                   [SCALING_LIST_SIZE_NUM][SCALING_LIST_NUM]; //!< RefMatrixID
+  std::vector<Int> m_scalingListCoef               [SCALING_LIST_SIZE_NUM][SCALING_LIST_NUM]; //!< quantization matrix
+};
+
+class ProfileTierLevel
+{
+  Int               m_profileSpace;
+  Level::Tier       m_tierFlag;
+  Profile::Name     m_profileIdc;
+  Bool              m_profileCompatibilityFlag[32];
+  Level::Name       m_levelIdc;
+
+  Bool              m_progressiveSourceFlag;
+  Bool              m_interlacedSourceFlag;
+  Bool              m_nonPackedConstraintFlag;
+  Bool              m_frameOnlyConstraintFlag;
+  UInt              m_bitDepthConstraintValue;
+  ChromaFormat      m_chromaFormatConstraintValue;
+  Bool              m_intraConstraintFlag;
+  Bool              m_onePictureOnlyConstraintFlag;
