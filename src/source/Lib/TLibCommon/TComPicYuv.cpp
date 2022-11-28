6447c1c7e4f08793316554c862ca7557c17d378c
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-11-28 20:43:21 +0800

    Fix issue causing incorrect calculation results

--- src/source/Lib/TLibCommon/TComPicYuv.cpp
+++ src/source/Lib/TLibCommon/TComPicYuv.cpp
@@ -398,3 +398,88 @@ Void TComPicYuv::extendPicBorder(const ComponentID compId, const Int marginX, co
       pi[width + x] = pi[width - 1];
     }
     pi += stride;
+  }
+
+  // pi is now the (0,height) (bottom left of image within bigger picture
+  pi -= (stride + marginX);
+  // pi is now the (-marginX, height-1)
+  for (Int y = 0; y < marginY; y++)
+  {
+    ::memcpy(pi + (y + 1) * stride, pi, sizeof(Pel) * (width + (marginX << 1)));
+  }
+
+  // pi is still (-marginX, height-1)
+  pi -= ((height - 1) * stride);
+  // pi is now (-marginX, 0)
+  for (Int y = 0; y < marginY; y++)
+  {
+    ::memcpy(pi - (y + 1) * stride, pi, sizeof(Pel) * (width + (marginX << 1)));
+  }
+}
+#endif
+
+
+// NOTE: This function is never called, but may be useful for developers.
+#if JVET_X0048_X0103_FILM_GRAIN
+Void TComPicYuv::dump(const std::string& fileName, const BitDepths& bitDepths, const Bool bAppend, const Bool bForceTo8Bit, const Bool bScaleMarginChroma) const
+#else
+Void TComPicYuv::dump (const std::string &fileName, const BitDepths &bitDepths, const Bool bAppend, const Bool bForceTo8Bit) const
+#endif
+{
+  FILE *pFile = fopen (fileName.c_str(), bAppend?"ab":"wb");
+
+  Bool is16bit=false;
+  for(Int comp = 0; comp < getNumberValidComponents() && !bForceTo8Bit; comp++)
+  {
+    if (bitDepths.recon[toChannelType(ComponentID(comp))]>8)
+    {
+      is16bit=true;
+    }
+  }
+
+  for(Int comp = 0; comp < getNumberValidComponents(); comp++)
+  {
+    const ComponentID  compId = ComponentID(comp);
+    const Pel         *pi     = getAddr(compId);
+#if JVET_X0048_X0103_FILM_GRAIN
+    const Int          stride = getStride(compId, bScaleMarginChroma);
+#else
+    const Int          stride = getStride(compId);
+#endif
+    const Int          height = getHeight(compId);
+    const Int          width  = getWidth(compId);
+
+    if (is16bit)
+    {
+      for (Int y = 0; y < height; y++ )
+      {
+        for (Int x = 0; x < width; x++ )
+        {
+          UChar uc = (UChar)((pi[x]>>0) & 0xff);
+          fwrite( &uc, sizeof(UChar), 1, pFile );
+          uc = (UChar)((pi[x]>>8) & 0xff);
+          fwrite( &uc, sizeof(UChar), 1, pFile );
+        }
+        pi += stride;
+      }
+    }
+    else
+    {
+      const Int shift  = bitDepths.recon[toChannelType(compId)] - 8;
+      const Int offset = (shift>0)?(1<<(shift-1)):0;
+      for (Int y = 0; y < height; y++ )
+      {
+        for (Int x = 0; x < width; x++ )
+        {
+          UChar uc = (UChar)Clip3<Pel>(0, 255, (pi[x]+offset)>>shift);
+          fwrite( &uc, sizeof(UChar), 1, pFile );
+        }
+        pi += stride;
+      }
+    }
+  }
+
+  fclose(pFile);
+}
+
+//! \}
