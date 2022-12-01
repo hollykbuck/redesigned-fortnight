3b615cc4d71c4debf46d2f05a722d841e256bfc2
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-12-01 20:43:22 +0800

    Fix broken unit test due to API changes

--- src/source/Lib/Utilities/program_options_lite.h
+++ src/source/Lib/Utilities/program_options_lite.h
@@ -98,3 +98,103 @@ namespace df
     };
 
     extern ErrorReporter default_error_reporter;
+
+    void doHelp(std::ostream& out, Options& opts, unsigned columns = 80);
+    std::list<const char*> scanArgv(Options& opts, unsigned argc, const char* argv[], ErrorReporter& error_reporter = default_error_reporter);
+    void setDefaults(Options& opts);
+    void parseConfigFile(Options& opts, const std::string& filename, ErrorReporter& error_reporter = default_error_reporter);
+
+    /** OptionBase: Virtual base class for storing information relating to a
+     * specific option This base class describes common elements.  Type specific
+     * information should be stored in a derived class. */
+    struct OptionBase
+    {
+      OptionBase(const std::string& name, const std::string& desc)
+      : opt_string(name), opt_desc(desc)
+      {};
+
+      virtual ~OptionBase() {}
+
+      /* parse argument arg, to obtain a value for the option */
+      virtual void parse(const std::string& arg, ErrorReporter&) = 0;
+      /* set the argument to the default value */
+      virtual void setDefault() = 0;
+
+      std::string opt_string;
+      std::string opt_desc;
+    };
+
+    /** Type specific option storage */
+    template<typename T>
+    struct Option : public OptionBase
+    {
+      Option(const std::string& name, T& storage, T default_val, const std::string& desc)
+      : OptionBase(name, desc), opt_storage(storage), opt_default_val(default_val)
+      {}
+
+      void parse(const std::string& arg, ErrorReporter&);
+
+      void setDefault()
+      {
+        opt_storage = opt_default_val;
+      }
+
+      T& opt_storage;
+      T opt_default_val;
+    };
+
+    /* Generic parsing */
+    template<typename T>
+    inline void
+    Option<T>::parse(const std::string& arg, ErrorReporter&)
+    {
+      std::istringstream arg_ss (arg,std::istringstream::in);
+      arg_ss.exceptions(std::ios::failbit);
+      try
+      {
+        arg_ss >> opt_storage;
+      }
+      catch (...)
+      {
+        throw ParseFailure(opt_string, arg);
+      }
+    }
+
+    /* string parsing is specialized -- copy the whole string, not just the
+     * first word */
+    template<>
+    inline void
+    Option<std::string>::parse(const std::string& arg, ErrorReporter&)
+    {
+      opt_storage = arg;
+    }
+
+    /** Option class for argument handling using a user provided function */
+    struct OptionFunc : public OptionBase
+    {
+      typedef void (Func)(Options&, const std::string&, ErrorReporter&);
+
+      OptionFunc(const std::string& name, Options& parent_, Func *func_, const std::string& desc)
+      : OptionBase(name, desc), parent(parent_), func(func_)
+      {}
+
+      void parse(const std::string& arg, ErrorReporter& error_reporter)
+      {
+        func(parent, arg, error_reporter);
+      }
+
+      void setDefault()
+      {
+        return;
+      }
+
+    private:
+      Options& parent;
+      Func* func;
+    };
+
+    class OptionSpecific;
+    struct Options
+    {
+      ~Options();
+
