979c6b346fd4eaeedbe3c1067fba88fe27dfa3c7
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2023-03-24 20:43:35 +0800

    Update error handling in file upload endpoint

--- src/source/Lib/TLibEncoder/WeightPredAnalysis.cpp
+++ src/source/Lib/TLibEncoder/WeightPredAnalysis.cpp
@@ -98,3 +98,103 @@ Void xCalcHistogram(const Pel  *pPel,
     pPel += stride;
   }
 }
+
+static
+Distortion xCalcHistDistortion (const std::vector<Int> &histogram0,
+                                const std::vector<Int> &histogram1)
+{
+  Distortion distortion = 0;
+  assert(histogram0.size()==histogram1.size());
+  const Int numElements=Int(histogram0.size());
+
+  // Scan histograms to compute histogram distortion
+  for (Int i = 0; i <= numElements; i++)
+  {
+    distortion += (Distortion)(abs(histogram0[i] - histogram1[i]));
+  }
+
+  return distortion;
+}
+
+static
+void xScaleHistogram(const std::vector<Int> &histogramInput,
+                           std::vector<Int> &histogramOutput, // cannot be the same as the input
+                     const Int               bitDepth,
+                     const Int               log2Denom,
+                     const Int               weight,
+                     const Int               offset,
+                     const Bool              bHighPrecision)
+{
+  assert(&histogramInput != &histogramOutput);
+  const Int numElements=Int(histogramInput.size());
+  histogramOutput.clear();
+  histogramOutput.resize(numElements);
+
+  const Int64 iRealLog2Denom = bHighPrecision ? 0 : (bitDepth - 8);
+  const Int64 iRealOffset    = ((Int64)offset)<<iRealLog2Denom;
+
+  const Int divOffset = log2Denom == 0 ? 0 : 1 << (log2Denom - 1);
+  // Scan histogram and apply illumination parameters appropriately
+  // Then compute updated histogram.
+  // Note that this technique only works with single list weights/offsets.
+
+  for (Int i = 0; i < numElements; i++)
+  {
+    const Int j = Clip3(0, numElements - 1, (Int)(((weight * i + divOffset) >> log2Denom) + iRealOffset));
+    histogramOutput[j] += histogramInput[i];
+  }
+}
+
+static
+Distortion xSearchHistogram(const std::vector<Int> &histogramSource,
+                            const std::vector<Int> &histogramRef,
+                                  std::vector<Int> &outputHistogram,
+                            const Int               bitDepth,
+                            const Int               log2Denom,
+                                  Int              &weightToUpdate,
+                                  Int              &offsetToUpdate,
+                            const Bool              bHighPrecision,
+                            const ComponentID       compID)
+{
+  const Int initialWeight   = weightToUpdate;
+  const Int initialOffset   = offsetToUpdate;
+  const Int weightRange     = 10;
+  const Int offsetRange     = 10;
+  const Int maxOffset       = 1 << ((bHighPrecision == true) ? (bitDepth - 1) : 7);
+  const Int range           = bHighPrecision ? (1<<bitDepth) / 2 : 128;
+  const Int defaultWeight   = (1<<log2Denom);
+  const Int minSearchWeight = std::max<Int>(initialWeight - weightRange, defaultWeight - range);
+  const Int maxSearchWeight = std::min<Int>(initialWeight + weightRange+1, defaultWeight + range);
+
+  Distortion minDistortion   = std::numeric_limits<Distortion>::max();
+  Int        bestWeight      = initialWeight;
+  Int        bestOffset      = initialOffset;
+
+  for (Int searchWeight = minSearchWeight; searchWeight < maxSearchWeight; searchWeight++)
+  {
+    if (compID == COMPONENT_Y)
+    {
+      for (Int searchOffset = std::max<Int>(initialOffset - offsetRange, -maxOffset);
+               searchOffset <= initialOffset + offsetRange && searchOffset<=(maxOffset-1);
+               searchOffset++)
+      {
+        xScaleHistogram(histogramRef, outputHistogram, bitDepth, log2Denom, searchWeight, searchOffset, bHighPrecision);
+        const Distortion distortion = xCalcHistDistortion(histogramSource, outputHistogram);
+
+        if (distortion < minDistortion)
+        {
+          minDistortion = distortion;
+          bestWeight    = searchWeight;
+          bestOffset    = searchOffset;
+        }
+      }
+    }
+    else
+    {
+      const Int pred        = ( maxOffset - ( ( maxOffset*searchWeight)>>(log2Denom) ) );
+
+      for (Int searchOffset = initialOffset - offsetRange; searchOffset <= initialOffset + offsetRange; searchOffset++)
+      {
+        const Int deltaOffset   = Clip3( -4*maxOffset, 4*maxOffset-1, (searchOffset - pred) ); // signed 10bit (if !bHighPrecision)
+        const Int clippedOffset = Clip3( -1*maxOffset, 1*maxOffset-1, (deltaOffset  + pred) ); // signed 8bit  (if !bHighPrecision)
+        xScaleHistogram(histogramRef, outputHistogram, bitDepth, log2Denom, searchWeight, clippedOffset, bHighPrecision);
