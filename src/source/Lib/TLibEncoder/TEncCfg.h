17e4aa31cbecaa55be7e6e1784c32450b4573edd
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2023-04-01 20:43:36 +0800

    Improve error reporting in error logs

--- src/source/Lib/TLibEncoder/TEncCfg.h
+++ src/source/Lib/TLibEncoder/TEncCfg.h
@@ -98,3 +98,103 @@ struct GOPEntry
   }
 };
 
+std::istringstream &operator>>(std::istringstream &in, GOPEntry &entry);     //input
+//! \ingroup TLibEncoder
+//! \{
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+/// encoder configuration class
+class TEncCfg
+{
+public:
+
+struct TEncSEIKneeFunctionInformation
+{
+  struct KneePointPair
+  {
+    Int inputKneePoint;
+    Int outputKneePoint;
+  };
+
+  Int       m_kneeFunctionId;
+  Bool      m_kneeFunctionCancelFlag;
+  Bool      m_kneeFunctionPersistenceFlag;
+  Int       m_inputDRange;
+  Int       m_inputDispLuminance;
+  Int       m_outputDRange;
+  Int       m_outputDispLuminance;
+  std::vector<KneePointPair> m_kneeSEIKneePointPairs;
+};
+
+#if JVET_T0050_ANNOTATED_REGIONS_SEI
+  std::map<UInt, SEIAnnotatedRegions::AnnotatedRegionObject> m_arObjects;
+#endif
+
+protected:
+  //==== File I/O ========
+  Int       m_iFrameRate;
+  Int       m_FrameSkip;
+  UInt      m_temporalSubsampleRatio;
+  Int       m_iSourceWidth;
+  Int       m_iSourceHeight;
+  Window    m_conformanceWindow;
+  Int       m_framesToBeEncoded;
+  Double    m_adLambdaModifier[ MAX_TLAYER ];
+  std::vector<Double> m_adIntraLambdaModifier;
+  Double    m_dIntraQpFactor;                                 ///< Intra Q Factor. If negative, use a default equation: 0.57*(1.0 - Clip3( 0.0, 0.5, 0.05*(Double)(isField ? (GopSize-1)/2 : GopSize-1) ))
+
+  Bool      m_printMSEBasedSequencePSNR;
+  Bool      m_printHexPsnr;
+  Bool      m_printFrameMSE;
+  Bool      m_printSequenceMSE;
+  Bool      m_printMSSSIM;
+  Bool      m_bXPSNREnableFlag;
+  Double    m_dXPSNRWeight[MAX_NUM_COMPONENT];
+  Bool      m_cabacZeroWordPaddingEnabled;
+#if SHUTTER_INTERVAL_SEI_PROCESSING
+  Bool      m_ShutterFilterEnable;                          ///< enable Pre-Filtering with Shutter Interval SEI
+#endif
+
+  /* profile & level */
+  Profile::Name m_profile;
+  Level::Tier   m_levelTier;
+  Level::Name   m_level;
+  Bool m_progressiveSourceFlag;
+  Bool m_interlacedSourceFlag;
+  Bool m_nonPackedConstraintFlag;
+  Bool m_frameOnlyConstraintFlag;
+  UInt              m_bitDepthConstraintValue;
+  ChromaFormat      m_chromaFormatConstraintValue;
+  Bool              m_intraConstraintFlag;
+  Bool              m_onePictureOnlyConstraintFlag;
+  Bool              m_lowerBitRateConstraintFlag;
+
+  //====== Coding Structure ========
+  UInt      m_uiIntraPeriod;                    // TODO: make this an Int - it can be -1!
+  UInt      m_uiDecodingRefreshType;            ///< the type of decoding refresh employed for the random access.
+  Bool      m_bReWriteParamSetsFlag;
+  Int       m_iGOPSize;
+  GOPEntry  m_GOPList[MAX_GOP];
+  Int       m_extraRPSs;
+  Int       m_maxDecPicBuffering[MAX_TLAYER];
+  Int       m_numReorderPics[MAX_TLAYER];
+
+  Int       m_iQP;                              //  if (AdaptiveQP == OFF)
+  Int       m_intraQPOffset;                    ///< QP offset for intra slice (integer)
+  Int       m_lambdaFromQPEnable;               ///< enable lambda derivation from QP
+  Int       m_sourcePadding[2];
+
+  Bool      m_AccessUnitDelimiter;               ///< add Access Unit Delimiter NAL units
+
+  Int       m_iMaxRefPicNum;                     ///< this is used to mimic the sliding mechanism used by the decoder
+                                                 // TODO: We need to have a common sliding mechanism used by both the encoder and decoder
+
+  Int       m_maxTempLayer;                      ///< Max temporal layer
+  Bool      m_useAMP;
+  UInt      m_maxCUWidth;
+  UInt      m_maxCUHeight;
+  UInt      m_maxTotalCUDepth;
+  UInt      m_log2DiffMaxMinCodingBlockSize;
